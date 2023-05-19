14fcf8ca84970f0881e61a2afecdd0c7f5db9aca
fd-Martin <fdmosharof@gmail.com>
2023-05-19 08:18:59 +0600

    69-6 Load Services fake data and display service Card

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 
 import Bannar from '../Bannar/Bannar';
 import About from '../About/About';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
             <Bannar></Bannar>
             <About></About>
+            <Services></Services>
         </div>
     );
 };
