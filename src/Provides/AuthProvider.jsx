afaba49e1e71dc0b032418fa846e07cd8bbf2d44
fd-Martin <fdmosharof@gmail.com>
2023-05-19 11:44:12 +0600

    69-9 (Recap) Create a user and Module Summary and Home work

--- src/Provides/AuthProvider.jsx
+++ src/Provides/AuthProvider.jsx
@@ -1,13 +1,39 @@
-import React, { createContext, useState } from 'react';
-import { getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
-export const AuthContext =createContext();
-const auth=getAuth(app);
-const AuthProvider = ({children}) => {
-    const [user,setUser]=useState(null);
-    const[loading,setLoading]=useState(true);
-    const authInfo={
-        user,loading
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn=(email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('current user', currentUser);
+            setLoading(false);
+        });
+        return()=>{
+            return unsubscribe();
+        }
+    }, [])
+   
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn
     }
     return (
         <AuthContext.Provider value={authInfo}>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,44 +1,57 @@
 import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
+import { useContext } from 'react';
+import { AuthContext } from '../../Provides/AuthProvider';
 const Login = () => {
-    const handleLogin=event=>{
+    const { signIn } = useContext(AuthContext);
+    const handleLogin = event => {
         event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log({ email, password })
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.log(error));
     }
     return (
         <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row">
-    <div className="w-1/2 mr-12">
-      <img src={img} alt="" />
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-      <h1 className="text-3xl text-center font-bold">Login now!</h1>
-        <form onSubmit={handleLogin}>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" name='email' className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password"  name='password' className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <input className="btn btn-primary" type="submit" value="Login" />
-        </div>
-        </form>
-        <p className='my-4 text-center'>new to car doctors <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="w-1/2 mr-12">
+                    <img src={img} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className="text-3xl text-center font-bold">Login now!</h1>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" name='email' className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" name='password' className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary" type="submit" value="Login" />
+                            </div>
+                        </form>
+                        <p className='my-4 text-center'>new to car doctors <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
 
-      </div>
-    </div>
-  </div>
-</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
