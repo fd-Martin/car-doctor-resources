3fc123056094736b0548c2b78c94af4d7709caf3
fd-Martin <fdmosharof@gmail.com>
2023-05-19 07:43:46 +0600

    69-5 Create About section with Image Relative Position

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+
 import Bannar from '../Bannar/Bannar';
+import About from '../About/About';
 
 const Home = () => {
     return (
         <div>
             <Bannar></Bannar>
+            <About></About>
         </div>
     );
 };
